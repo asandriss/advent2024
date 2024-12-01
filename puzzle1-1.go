32e7bf3eff04e077f00f7626a63884e4c85a0050
Fedor Hajdu <fedorhajdu@gmail.com>
2024-12-01 18:03:59 +0000

    formatting

--- puzzle1-1.go
+++ puzzle1-1.go
@@ -3,48 +3,48 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"math"
 	"os"
-  "math"
 	"slices"
-  "strconv"
+	"strconv"
 	"strings"
 )
 
 func main() {
-  file, err := os.Open("puzzle1-1.input.txt")
-  if err != nil {
-    fmt.Println("error opening file", err)
-    return
-  }
+	file, err := os.Open("puzzle1-1.input.txt")
+	if err != nil {
+		fmt.Println("error opening file", err)
+		return
+	}
 
-  defer file.Close()
+	defer file.Close()
 
-  var first  []int
-  var second []int
+	var first []int
+	var second []int
 
-  scanner := bufio.NewScanner(file)
-  for scanner.Scan() {
-    line := scanner.Text()
-    strings := strings.Fields(line)
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		strings := strings.Fields(line)
 
-    if len(strings) == 0 {
-      break
-    }
-    
-    no1, _ := strconv.Atoi(strings[0])
-    no2, _ := strconv.Atoi(strings[1])
+		if len(strings) == 0 {
+			break
+		}
 
-    first = append(first, no1) 
-    second = append(second, no2)
-  }
+		no1, _ := strconv.Atoi(strings[0])
+		no2, _ := strconv.Atoi(strings[1])
 
-  slices.Sort(first)
-  slices.Sort(second)
-  
-  runningTotal := 0
-  for i := 0; i < len(first); i++ {
-    runningTotal += int(math.Abs(float64(first[i]-second[i]))) 
-  }
+		first = append(first, no1)
+		second = append(second, no2)
+	}
 
-  fmt.Println("Running total", runningTotal)
+	slices.Sort(first)
+	slices.Sort(second)
+
+	runningTotal := 0
+	for i := 0; i < len(first); i++ {
+		runningTotal += int(math.Abs(float64(first[i] - second[i])))
+	}
+
+	fmt.Println("Running total", runningTotal)
 }
